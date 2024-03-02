e3d25d3541e55468c6a1727357d1a8b9435d55ba
RosenValev <121716535+RosenValev@users.noreply.github.com>
2024-03-02 21:28:42 +0200

    update authmiddleware

--- server/utils/authMiddleware.js
+++ server/utils/authMiddleware.js
@@ -1,16 +1,16 @@
-const jwt = require('jsonwebtoken');
+const jwt = require('./jwt.js')
 
 module.exports = async (req, res, next) => {
-    // const token = req.cookies.token;
-    const token = req.headers.auth;
+    const token = req.cookies.auth;
+    // const token = req.headers.auth;
 
     try {
-        const data = await jwt.verify(token, process.env.JWT_SECRET);
+        const data = await jwt.verifyToken(token, process.env.JWT_SECRET);
         req.user = data;
         req.isLogged = true;
     } catch (err) {
-        // res.clearCookie(token);
-        return res.status(403).json({ message: 'Invalid token!' });
+        res.clearCookie(token);
+        return res.status(401).json({ message: 'Invalid token!' });
     }
     next();
 };
