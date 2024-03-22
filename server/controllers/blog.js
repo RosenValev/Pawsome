4882f7fdb572890d94887b2c22daae622bce0647
RosenValev <121716535+RosenValev@users.noreply.github.com>
2024-03-22 16:05:36 +0200

    add getBlogPostById to blogController

--- server/controllers/blog.js
+++ server/controllers/blog.js
@@ -2,7 +2,7 @@ const Blog = require('../models/Blog.js');
 const jwt = require('../utils/jwt.js');
 
 
-const createPost = async (req, res) => {
+const createBlogPost = async (req, res) => {
     try {
         const { title, subTitle, imageUrl, text, token } = req.body
         if (!title || !subTitle || !imageUrl || !text || !token) {
@@ -29,9 +29,26 @@ const getAllBlogPosts = async (req, res) => {
     }
 }
 
+const getBlogPostById = async (req, res) => {
+    const { id } = req.params;
+    try {
+        if (!id) {
+            return res.status(404).json({ message: `Id is required` });
+        }
+        const blogPost = await Blog.findById(id).populate('owner');
+        if (!blogPost) {
+            return res.status(404).json({ message: `Blog not found` });
+        }
+        res.status(200).json(blogPost);
+    } catch (error) {
+        next(error)
+    }
+}
+
 
 
 module.exports = {
-    createPost,
-    getAllBlogPosts
+    createBlogPost,
+    getAllBlogPosts,
+    getBlogPostById
 }
--- server/routes/blogRouter.js
+++ server/routes/blogRouter.js
@@ -3,6 +3,7 @@ const blogController = require('../controllers/blog.js');
 const authMiddleware = require('../utils/authMiddleware.js');
 
 blogRouter.get('/', blogController.getAllBlogPosts);
-blogRouter.post('/create', authMiddleware, blogController.createPost);
+blogRouter.get('/:id', blogController.getBlogPostById);
+blogRouter.post('/create', authMiddleware, blogController.createBlogPost);
 
 module.exports = blogRouter;
