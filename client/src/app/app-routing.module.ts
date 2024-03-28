e2917d42293bcbb57d0cb7665d258b410e31ce2f
RosenValev <121716535+RosenValev@users.noreply.github.com>
2024-03-28 14:48:16 +0200

    Update page-services component

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { ErrorComponent } from './page-not-found/error.component';
+import { PageServicesComponent } from './page-services/page-services.component';
 
 const routes: Routes = [
   { path: '', pathMatch: 'full', redirectTo: '/home' },
   { path: 'home', component: HomeComponent },
+  { path: 'services', component: PageServicesComponent },
   { path: '**', redirectTo: '404' },
   { path: '404', component: ErrorComponent },
 ];
